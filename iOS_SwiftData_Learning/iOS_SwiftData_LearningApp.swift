1e827f1a2fdc2ecb31bba9c720a7fc705b506e10
Saikaku <grnmnsta@gmail.com>
2025-05-26 09:38:03 +0900

    Initial Commit

---
+++ iOS_SwiftData_Learning/iOS_SwiftData_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SwiftData_LearningApp.swift
+//  iOS_SwiftData_Learning
+//
+//  Created by cmStudent on 2025/05/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SwiftData_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
